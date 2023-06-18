e8d874457d9d759d60e056d2cdc05ceb16ec2e27
Rodrigo Balbino <rodrigo080599@hotmail.com>
2023-06-17 23:33:00 -0300

    remove typescript
    Co-authored-by: antonioaldisio <aldisiofilho@gmail.com>

--- frontend/src/ts/app.ts
+++
@@ -1,9 +0,0 @@
-import axios, {AxiosResponse} from 'axios';
-
-
-axios.get("http://172.16.1.194:9944/info/23.6").then((res: AxiosResponse) => {
-    console.log(res.data);
-});
-
-console.log("Hello");
-
