9ab3d6c0855cae51151b214940cc658552c06095
Rodrigo Balbino <rodrigo080599@hotmail.com>
2023-06-20 21:34:10 -0300

    return values on back
    Co-authored-by: antonioaldisio <aldisiofilho@gmail.com>

--- backend/mediana.py
+++ backend/mediana.py
@@ -34,8 +34,57 @@ def informacao(valor_desejado):
     df['Última (R$)'] = df['Última (R$)'].astype(float)
     df['Última (R$)'] = df['Última (R$)']/100
     closest_price = median_of_medians(df['Última (R$)'].tolist(), valor_desejado)
-    row = df[df['Última (R$)'] == closest_price]
-    codigo = row['Código'].values[0]
-    nome = row['Nome'].values[0]
-    valor = row['Última (R$)'].values[0]
-    return {"Codigo": codigo, "Nome": nome, "Valor": valor}
+   
+    lista = sorted(df['Última (R$)'])
+    posicao = lista.index(closest_price)
+    valores_proximos = lista[max(0, posicao-2):posicao+3]
+
+    while len(valores_proximos) < 4:
+        if posicao - 2 < 0:  
+            valores_proximos.append(lista[posicao+len(valores_proximos)+1])
+        elif posicao + len(valores_proximos) >= len(lista): 
+            valores_proximos.insert(0, lista[posicao-len(valores_proximos)-1])
+        else:
+            valores_proximos.append(lista[posicao+len(valores_proximos)+1])
+            valores_proximos.insert(0, lista[posicao-len(valores_proximos)-1])
+
+    row0 = df[df['Última (R$)'] == valores_proximos[0]]
+    row1 = df[df['Última (R$)'] == valores_proximos[1]]
+    row2 = df[df['Última (R$)'] == valores_proximos[2]]
+    row3 = df[df['Última (R$)'] == valores_proximos[3]]
+    row4 = df[df['Última (R$)'] == valores_proximos[4]]
+
+    cod0 = row0["Código"].values[0]
+    nom0 = row0["Nome"].values[0]
+    valo0 = row0["Última (R$)"].values[0]
+
+    cod1 = row1["Código"].values[0]
+    nom1 = row1["Nome"].values[0]
+    valo1 = row1["Última (R$)"].values[0]
+
+    cod2 = row2["Código"].values[0]
+    nom2 = row2["Nome"].values[0]
+    valo2 = row2["Última (R$)"].values[0]
+
+    cod3 = row3["Código"].values[0]
+    nom3 = row3["Nome"].values[0]
+    valo3 = row3["Última (R$)"].values[0]
+
+    cod4 = row4["Código"].values[0]
+    nom4 = row4["Nome"].values[0]
+    valo4 = row4["Última (R$)"].values[0]
+
+    return {
+    "Codigo0":cod0, "Nome0":nom0, "Valor0":valo0,
+    "Codigo1":cod1, "Nome1":nom1, "Valor1":valo1,
+    "Codigo2":cod2, "Nome2":nom2, "Valor2":valo2,
+    "Codigo3":cod3, "Nome3":nom3, "Valor3":valo3,
+    "Codigo4":cod4, "Nome4":nom4, "Valor4":valo4,
+    }
+
+   # row = df[df['Última (R$)'] == closest_price]
+   # codigo = row['Código'].values[0]
+   # nome = row['Nome'].values[0]
+   # valor = row['Última (R$)'].values[0]
+   # return {"Codigo": codigo, "Nome": nome, "Valor": valor}
+  
