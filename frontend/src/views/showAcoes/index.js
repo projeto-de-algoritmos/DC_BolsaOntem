83b3ea57d0b793978d9dc8c8195016d3dba39795
Antonio Aldisio <aldisiofilho@gmail.com>
2023-06-20 22:47:37 -0300

    add: escolha do algoritimo
    Co-authored-by: Rodrigo Balbino <rodrigo080599@hotmail.com>

--- frontend/src/views/showAcoes/index.js
+++ frontend/src/views/showAcoes/index.js
@@ -5,12 +5,22 @@ import './index.css';
 function ShowAcoes() {
   const navigate = useNavigate();
   const [response, setResponse] = useState(null);
+  const [duplicateIndex, setDuplicateIndex] = useState(null);
 
   useEffect(() => {
     const storedResponse = localStorage.getItem('response');
     if (storedResponse) {
       const parsedResponse = JSON.parse(storedResponse);
-      setResponse(parsedResponse);
+      if (Array.isArray(parsedResponse)) {
+        const filteredResponse = parsedResponse.filter(
+          item => item.Codigo && item.Nome && item.Valor !== null
+        );
+        const sortedResponse = filteredResponse.sort((a, b) => a.Valor - b.Valor);
+        setResponse(sortedResponse);
+        const valueToCompare = parsedResponse[5]?.Valor;
+        const duplicateIndex = sortedResponse.findIndex(item => item.Valor === valueToCompare);
+        setDuplicateIndex(duplicateIndex);
+      }
     }
   }, []);
 
@@ -22,21 +32,30 @@ function ShowAcoes() {
     <div className='body'>
       <div className="back">
         <h1 className="acoes-title-title">Resultado da busca de ações</h1>
-        <ul className="acoes-list">
-          <h2 className="acoes-title">Ações encontradas:</h2>
-          <li className="acoes-header">
-            <span>Código</span>
-            <span>Nome</span>
-            <span>Última(R$)</span>
-          </li>
-          {response && (
-            <li className="acoes-item">
-              <span>{response.Codigo}</span>
-              <span>{response.Nome}</span>
-              <span>{response.Valor}</span>
+        {response && response.length > 0 ? (
+          <ul className="acoes-list">
+            <h2 className="acoes-title">Ações encontradas:</h2>
+            <li className="acoes-header">
+              <span>Código</span>
+              <span>Nome</span>
+              <span>Última(R$)</span>
             </li>
-          )}
-        </ul>
+            {response.map((item, index) => (
+              item.Nome !== null && (
+                <li
+                  className={`acoes-item ${index === duplicateIndex ? 'duplicate' : ''}`}
+                  key={item.Codigo}
+                >
+                  <span>{item.Codigo}</span>
+                  <span>{item.Nome}</span>
+                  <span>{item.Valor}</span>
+                </li>
+              )
+            ))}
+          </ul>
+        ) : (
+          <p>Nenhuma ação encontrada.</p>
+        )}
         <button className="button" onClick={handleGoBackBolsa}>
           Procurar outros valores
         </button>
